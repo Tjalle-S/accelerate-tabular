09401a490764e9463924a0ea4508d85433b568a9
Tjalle-S <91267211+Tjalle-S@users.noreply.github.com>
2025-11-17 20:35:43 +0100

    Setup stack project

---
+++ app/Main.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE TypeApplications #-}
+
+module Main (main) where
+
+import Data.Array.Accelerate
+import Data.Array.Accelerate.LLVM.Native
+
+import qualified Prelude
+
+main :: Prelude.IO ()
+main = do
+  let xs = map (+1) $ generate (I1 100) (\(I1 i) -> i * 3 + 1)
+  Prelude.putStrLn $ test @UniformScheduleFun @NativeKernel xs
+  Prelude.putStrLn $ Prelude.show $ run xs
