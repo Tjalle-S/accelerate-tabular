8c793738bf179bb3f2998e1c02b2357c6bd38ac9
Tjalle-S <91267211+Tjalle-S@users.noreply.github.com>
2026-03-03 18:12:33 +0100

    Change underlying Hashed represenation
    Now uses a segmented vector of fixed-width hash maps, for use with open addressing.

--- src/Data/Array/Accelerate/Tabular/Rep/Hashed.hs
+++ src/Data/Array/Accelerate/Tabular/Rep/Hashed.hs
@@ -3,8 +3,10 @@
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RebindableSyntax      #-}
-{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE TypeOperators         #-} 
+{-# LANGUAGE PatternSynonyms       #-}
 
+{-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 
 module Data.Array.Accelerate.Tabular.Rep.Hashed (
@@ -12,20 +14,31 @@ module Data.Array.Accelerate.Tabular.Rep.Hashed (
 ) where
 
 import Data.Array.Accelerate
-import Data.Array.Accelerate.Data.HashMap ( HashMap, Hashable, fromVector )
+import Data.Array.Accelerate.Data.HashMap ( Hashable )
 
 import Data.Array.Accelerate.Tabular.Classes.Rep
 import Data.Array.Accelerate.Tabular.Rep.Snoc
 import Data.Array.Accelerate.Tabular.Util
 
 -- | Stores keys in a hash map.
--- Can only be used for the outermost dimension.
 --
 data Hashed
 
-instance (Hashable key) => Rep (Z :.: Hashed) (Z :.: key) where
+instance (Rep rep keys, Hashable key) =>
+  Rep (rep :.: Hashed) (keys :.: key) where
 
-  type MetaR (Z :.: Hashed) (Z :.: key) = HashMap key Int
+  type MetaR (rep :.: Hashed) (keys :.: key) =
+    (Meta rep keys, Scalar Int, Vector key)
 
-  emptyMeta = Meta_ (fromVector emptyVector)
+  emptyMeta = HashedMeta emptyMeta (unit 0) emptyVector
   
+-- Local utilities.
+-- ----------------
+
+pattern HashedMeta :: (Arrays (Meta rep keys), Hashable key)
+                       => Acc (Meta rep keys)
+                       -> Acc (Scalar Int)
+                       -> Acc (Vector key)
+                       -> Acc (Meta (rep :.: Hashed) (keys :.: key))
+pattern HashedMeta { met, width, ks } = Meta_ (T3 met width ks)
+{-# COMPLETE HashedMeta #-}
