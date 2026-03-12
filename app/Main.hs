cc330e72931fcc2c6f79c3147bec113ac550bed3
Tjalle-S <91267211+Tjalle-S@users.noreply.github.com>
2026-03-12 16:28:55 +0100

    Start hashmap implementation

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,13 @@ import qualified Prelude
 
 main :: Prelude.IO ()
 main = do
-  let xs = map (+1) $ generate (I1 100) (\(I1 i) -> i * 3 + 1)
-  Prelude.putStrLn $ inspectCompiler @Native xs
-  Prelude.putStrLn $ Prelude.show $ run xs
+  Prelude.putStrLn (inspectCompiler @Native (split (even @Int)))
+  -- let xs = map (+1) $ generate (I1 100) (\(I1 i) -> i * 3 + 1)
+  -- Prelude.putStrLn $ inspectCompiler @Native xs
+  -- Prelude.putStrLn $ Prelude.show $ run xs
+
+split :: (Elt a)
+      => (Exp a -> Exp Bool)
+      -> Acc (Vector a)
+      -> Acc (Vector a, Vector a)
+split f xs = lift (afst $ filter f xs, afst $ filter (not . f) xs)
--- src/Data/Array/Accelerate/Tabular/Rep/Hashed.hs
+++ src/Data/Array/Accelerate/Tabular/Rep/Hashed.hs
@@ -3,22 +3,25 @@
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RebindableSyntax      #-}
-{-# LANGUAGE TypeOperators         #-} 
+{-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE PatternSynonyms       #-}
 
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Data.Array.Accelerate.Tabular.Rep.Hashed (
   Hashed
 ) where
 
 import Data.Array.Accelerate
-import Data.Array.Accelerate.Data.HashMap ( Hashable )
 
 import Data.Array.Accelerate.Tabular.Classes.Rep
 import Data.Array.Accelerate.Tabular.Rep.Snoc
 import Data.Array.Accelerate.Tabular.Util
+import Data.Array.Accelerate.Data.Hashable (Hashable(hash))
+import Data.Array.Accelerate.Data.Maybe (isNothing)
 
 -- | Stores keys in a hash map.
 --
@@ -30,15 +33,60 @@ instance (Rep rep keys, Hashable key) =>
   type MetaR (rep :.: Hashed) (keys :.: key) =
     (Meta rep keys, Scalar Int, Vector key)
 
-  emptyMeta = HashedMeta emptyMeta (unit 0) emptyVector
-  
+  emptyMeta = Meta_ $ T3 emptyMeta (unit 0) emptyVector
+
 -- Local utilities.
 -- ----------------
 
-pattern HashedMeta :: (Arrays (Meta rep keys), Hashable key)
-                       => Acc (Meta rep keys)
-                       -> Acc (Scalar Int)
-                       -> Acc (Vector key)
-                       -> Acc (Meta (rep :.: Hashed) (keys :.: key))
-pattern HashedMeta { met, width, ks } = Meta_ (T3 met width ks)
-{-# COMPLETE HashedMeta #-}
+-- pattern HashedMeta :: (Arrays (Meta rep keys), Hashable key)
+--                        => Acc (Meta rep keys)
+--                        -> Acc (Scalar Int)
+--                        -> Acc (Vector key)
+--                        -> Acc (Meta (rep :.: Hashed) (keys :.: key))
+-- pattern HashedMeta { met, width, ks } = Meta_ (T3 met width ks)
+-- {-# COMPLETE HashedMeta #-}
+
+
+
+
+data HashSet key = HashSet_ (Vector (Maybe key)) (Scalar Int)
+  deriving (Generic, Show)
+
+instance (Elt key) => Arrays (HashSet key)
+
+
+pattern HashSet :: (Elt key)
+                => Acc (Vector (Maybe key))
+                -> Acc (Scalar Int)
+                -> Acc (HashSet key)
+pattern HashSet { keys, width } = Pattern (keys, width)
+{-# COMPLETE HashSet #-}
+
+insert :: (Hashable key) => Acc (Vector (Int, key))
+                         -> Acc (HashSet key)
+                         -> Acc (HashSet key)
+insert sks (HashSet { keys, width }) =
+  let w = the width
+      -- start = seg * w
+
+      -- hks = map hash ks
+  in  undefined
+  where
+    condition :: (Elt key) => Acc (Scalar Int, Vector (Int, key), Vector (Maybe key)) -> Acc (Scalar Bool)
+    condition (T3 i todo _) = unit $ (the i < the width) && not (null todo)
+
+    step :: (Hashable key) => Acc (Scalar Int, Vector (Int, key), Vector (Maybe key)) -> Acc (Scalar Int, Vector (Int, key), Vector (Maybe key))
+    step (T3 i todo ks) = 
+      let f (T2 b k) = (hash k `mod` the width) + (the width * b)
+          is = map f todo
+          (good, coll) = undefined
+
+      in  undefined
+
+split :: (Elt a)
+      => (Exp a -> Exp Bool)
+      -> Acc (Vector a)
+      -> (Acc (Vector a), Acc (Vector a))
+split f xs = (afst $ filter f xs, afst $ filter (not . f) xs)
+
+-- insert' :: Acc (Scalar Int) -> Acc (Vector (Maybe key))
