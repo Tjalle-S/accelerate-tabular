99d88fa2ec8b64736fc1682865f84dc37857f1ab
Tjalle-S <91267211+Tjalle-S@users.noreply.github.com>
2026-03-20 11:19:14 +0100

    Hashset insert

--- src/Data/Array/Accelerate/Tabular/Rep/Hashed.hs
+++ src/Data/Array/Accelerate/Tabular/Rep/Hashed.hs
@@ -13,6 +13,7 @@
 
 module Data.Array.Accelerate.Tabular.Rep.Hashed (
   Hashed
+, emptyHashSet, insert, HashSet (..)
 ) where
 
 import Data.Array.Accelerate
@@ -21,7 +22,7 @@ import Data.Array.Accelerate.Tabular.Classes.Rep
 import Data.Array.Accelerate.Tabular.Rep.Snoc
 import Data.Array.Accelerate.Tabular.Util
 import Data.Array.Accelerate.Data.Hashable (Hashable(hash))
-import Data.Array.Accelerate.Data.Maybe (isNothing)
+import Data.Array.Accelerate.Data.Maybe (maybe)
 
 -- | Stores keys in a hash map.
 --
@@ -62,31 +63,69 @@ pattern HashSet :: (Elt key)
 pattern HashSet { keys, width } = Pattern (keys, width)
 {-# COMPLETE HashSet #-}
 
-insert :: (Hashable key) => Acc (Vector (Int, key))
-                         -> Acc (HashSet key)
-                         -> Acc (HashSet key)
-insert sks (HashSet { keys, width }) =
-  let w = the width
-      -- start = seg * w
+emptyHashSet :: (Elt key) => Exp Int -> Exp Int -> Acc (HashSet key)
+emptyHashSet n w =
+  let ks = fill (I1 $ n * w) Nothing_
+  in  HashSet ks (unit w)
 
-      -- hks = map hash ks
-  in  undefined
+insert :: (Eq key, Hashable key)
+       => Acc (Vector (Int, key))
+       -> Acc (HashSet key)
+       -> (Acc (HashSet key), Exp Int)
+insert sks (HashSet { keys, width }) =
+  let hks = map (\(T2 b k) -> T3 b (hash k) k) sks
+      T3 _ todo' keys' = awhile condition step (T3 (unit 0) hks keys)
+  in  (HashSet keys' width, length todo')
   where
-    condition :: (Elt key) => Acc (Scalar Int, Vector (Int, key), Vector (Maybe key)) -> Acc (Scalar Bool)
+    condition :: (Elt key)
+              => Acc (Scalar Int, Vector (Int, Int, key), Vector (Maybe key)) -> Acc (Scalar Bool)
     condition (T3 i todo _) = unit $ (the i < the width) && not (null todo)
 
-    step :: (Hashable key) => Acc (Scalar Int, Vector (Int, key), Vector (Maybe key)) -> Acc (Scalar Int, Vector (Int, key), Vector (Maybe key))
+    step :: (Eq key)
+         => Acc (Scalar Int, Vector (Int, Int, key), Vector (Maybe key))
+         -> Acc (Scalar Int, Vector (Int, Int, key), Vector (Maybe key))
     step (T3 i todo ks) = 
-      let f (T2 b k) = (hash k `mod` the width) + (the width * b)
-          is = map f todo
-          (good, coll) = undefined
+      let -- Find where to attempt insertion.
+          f (T3 b p _) = (p `mod` the width) + (the width * b)
+          ps = map f todo
 
-      in  undefined
+          -- Find collisions with already inserted keys.
+          mks = gather ps ks
+          (coll, good) = split
+            (\x -> isCollision (thd3 $ fst3 x) (thd3 x))
+            (zipChecked3 todo ps mks)
+          (goodTodo, goodps, _) = unzip3 good
+          (_, _, goodks) = unzip3 goodTodo
+
+          -- Insert if no collision.
+          ks' = scatter goodps ks (map Just_ goodks)
+
+          -- Find collisions within todo-list:
+          -- if key not present in new array, there was a collision.
+          mks' = gather goodps ks'
+          goodTodo' = map fst $ afst $ filter
+            (\x -> isCollision (thd3 $ fst x) (snd x))
+            (zipChecked goodTodo mks')
+
+          -- Make new todo-list. 
+          (collTodo, _, _) = unzip3 coll
+          todo' = map nextInSequence (collTodo ++ goodTodo')
+
+      in  T3 (map (+ 1) i) todo' ks'
+      where
+        nextInSequence :: (Elt key) => Exp (Int, Int, key) -> Exp (Int, Int, key)
+        nextInSequence (T3 b p k) = T3 b (p + 1) k
 
 split :: (Elt a)
       => (Exp a -> Exp Bool)
       -> Acc (Vector a)
       -> (Acc (Vector a), Acc (Vector a))
 split f xs = (afst $ filter f xs, afst $ filter (not . f) xs)
+
+isCollision :: (Eq a)
+            => Exp a
+            -> Exp (Maybe a)
+            -> Exp Bool
+isCollision k = maybe False_ (/= k)
 
 -- insert' :: Acc (Scalar Int) -> Acc (Vector (Maybe key))
--- src/Data/Array/Accelerate/Tabular/Util.hs
+++ src/Data/Array/Accelerate/Tabular/Util.hs
@@ -1,5 +1,6 @@
 module Data.Array.Accelerate.Tabular.Util (
   emptyVector
+, fst3, snd3, thd3
 ) where
 
 import Data.Array.Accelerate
@@ -8,3 +9,12 @@ import Data.Array.Accelerate.Unsafe (undef)
 -- | Creates an empty (length-0) vector.
 emptyVector :: (Elt e) => Acc (Vector e)
 emptyVector = fill (I1 0) undef
+
+fst3 :: (Elt a, Elt b, Elt c) => Exp (a, b, c) -> Exp a
+fst3 (T3 x _ _) = x
+
+snd3 :: (Elt a, Elt b, Elt c) => Exp (a, b, c) -> Exp b
+snd3 (T3 _ y _) = y
+
+thd3 :: (Elt a, Elt b, Elt c) => Exp (a, b, c) -> Exp c
+thd3 (T3 _ _ z) = z
