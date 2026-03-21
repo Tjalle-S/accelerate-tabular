fc323498ceb0f20eecee6c72894df33a4169045b
Tjalle-S <91267211+Tjalle-S@users.noreply.github.com>
2026-03-21 17:23:11 +0100

    First singleton version

--- app/Main.hs
+++ app/Main.hs
@@ -31,18 +31,20 @@ type Sparse = Z :.: OrdCompressed
 type H = Z :.: Hashed
 type HC = Z :.: Hashed :.: OrdCompressed
 
+type COO = Z :.: OrdCompressed :.: UnsafeCompleteSingleton
+
 main :: Prelude.IO ()
 main = let 
           --  vs = [1.0, 3.0, 5.0, 7.0]
 
-           d1s = [2,   12,   12,   2,   5,   8,   24]
+           d1s = [2,   21,   12,   1,   5,   8,   24]
            d2s = [3,   1,    0,    4,   1,   5,   3]
-           vs  = [2.3, 12.1, 12.0, 2.4, 5.1, 8.5, 24.3]
+           vs  = [2.3, 21.1, 12.0, 1.4, 5.1, 8.5, 24.3]
 
            ks = zipWith (\d1 d2 -> Z_ ::.: d1 ::.: d2) (use d1s) (use d2s)
            kvs = zip ks (use vs)
 
-           vec = createTable @HC @I2 @Float $ kvs
+           vec = createTable @COO @I2 @Float $ kvs
 
 
        in  Prelude.print $ run vec
--- src/Data/Array/Accelerate/Tabular/Rep/Singleton.hs
+++ src/Data/Array/Accelerate/Tabular/Rep/Singleton.hs
@@ -5,12 +5,13 @@
 {-# LANGUAGE RebindableSyntax      #-}
 {-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE PatternSynonyms       #-}
+{-# LANGUAGE NamedFieldPuns        #-}
 
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 
 module Data.Array.Accelerate.Tabular.Rep.Singleton (
-  Singleton
+  Singleton, UnsafeCompleteSingleton
 ) where
 
 import Data.Array.Accelerate
@@ -18,10 +19,15 @@ import Data.Array.Accelerate
 import Data.Array.Accelerate.Tabular.Classes.Rep
 import Data.Array.Accelerate.Tabular.Rep.Snoc
 import Data.Array.Accelerate.Tabular.Util
+import Data.Array.Accelerate.Unsafe (undef)
 
 -- | Stores a single key on this level for each key in the parent levels.
 --
 data Singleton
+
+-- | Stores a single key on this level for each key in the parent levels.
+--
+data UnsafeCompleteSingleton
 
 -- Singleton instances.
 -- --------------------
@@ -31,6 +37,27 @@ instance (Rep rep keys, Elt key) => Rep (rep :.: Singleton) (keys :.: key) where
   type MetaR (rep :.: Singleton) (keys :.: key) = (Meta rep keys, Vector key)
 
   emptyMeta = SingletonMeta emptyMeta emptyVector
+
+instance (Rep rep keys, Elt key) =>
+  Rep (rep :.: UnsafeCompleteSingleton) (keys :.: key) where
+
+    type MetaR (rep :.: UnsafeCompleteSingleton) (keys :.: key) =
+      (Meta rep keys, Vector key)
+
+    emptyMeta = Meta_ $ T2 emptyMeta emptyVector
+
+    createMeta ks =
+      let
+        (ks', is) = splitKeys ks
+        (met, perm, n) = createMeta ks'
+
+        perm' = map unindex1 perm
+        target = fill (I1 n) undef
+
+        met' = Meta_ $ T2 met (scatter perm' target is)
+      in (met', perm, n)
+
+
 
 
 -- Local utilities.
