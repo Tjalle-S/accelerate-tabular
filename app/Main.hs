1931ae463be0070c7417f7cabb01418e0728eb2e
Tjalle-S <91267211+Tjalle-S@users.noreply.github.com>
2026-06-01 11:54:01 +0200

    Attempts

--- app/Main.hs
+++ app/Main.hs
@@ -17,7 +17,7 @@
 {-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# OPTIONS_GHC -Wno-redundant-constraints #-}
-{-# LANGUAGE UndecidableSuperClasses #-}
+-- {-# LANGUAGE UndecidableSuperClasses #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE DeriveGeneric #-}
@@ -81,7 +81,7 @@ type Key = Z :. Int :. Int
 inf :: Exp Float
 inf = 1 / 0
 
-apsp :: forall rep . (Fold' rep Key) => Acc (Table rep Key Float) -> Acc (Table rep Key Float)
+apsp :: forall rep . (F rep Key) => Acc (Table rep Key Float) -> Acc (Table rep Key Float)
 apsp ds = afor (A.unit n) update ds
   where
     Z_ ::. n' ::. n'' = A.the $ A.maximum $ keys ds
@@ -89,8 +89,8 @@ apsp ds = afor (A.unit n) update ds
 
     update :: Acc (A.Scalar Int) -> Acc (Table rep Key Float) -> Acc (Table rep Key Float)
     update ak d = let k = A.the ak
-                      test = fold' (Keep :. Group) (+) 0 d
-                      test2 = map (+ 2) test--fold' (Keep :. Group) (+) 0 test
+                      test = fold'' (Keep :. Group) (+) 0 d
+                      -- test2 = map (+ 2) test--fold' (Keep :. Group) (+) 0 test
                       -- should be slice instead of filter
                       -- toK   = slice (Z_ ::. Keep_    ::. Slice_ k) d
                       -- fromK = slice (Z_ ::. Slice_ k ::. Keep_)    d
@@ -106,56 +106,119 @@ afor n f x = asnd $ awhile
   (\(T2 i x') -> T2 (A.map (+ 1) i) (f i x'))
   (T2 (A.unit 0) x)
 
-class (Rep rep key, IsKey key, RepShape rep key ~ KeyShape key) => Fold' rep key where
+
+class (FKey key, Rep rep key, Rep (FRRes rep) (FKRes key)) => F rep key where
+
+  type FRRes rep
+
+class (Eq key, Eq (FKRes key)) => FKey key where
+
+  type FKRes key
+
+instance FKey Z where
+
+  type FKRes Z = Z
+
+instance F Z Z where
+
+  type FRRes Z = Z
+
+instance (FKey keys, Eq key) => FKey (keys :. key) where
+
+  type FKRes (keys :. key) = keys
+
+instance (F rep keys, IndexKey key) => F (rep :. Dense) (keys :. key) where
+
+  type FRRes (rep :. Dense) = rep
+
+-- ----------------
+
+class (F rep key, Rep (FRRes' rep desc) (FKRes' key desc)) => FDesc rep key desc where
+
+  type FRRes' rep desc
+  type FKRes' key desc
+
+  getDesc :: FDesc' rep key desc
+
+instance (F rep key) => FDesc rep key Keep where
+
+  type FRRes' rep Keep = rep
+  type FKRes' key Keep = key
+
+  getDesc = FKeep
+
+-- instance (FDesc rep key) => FDesc rep 
+
+
+
+
+data FDesc' rep key desc where
+  FKeep :: FDesc' rep key Keep
+  FGroup :: (FDesc rep keys desc)
+         => FDesc' rep keys desc
+         -> FDesc' (rep :. r) (keys :. key) (desc :. Group)
+
+
+fold'' :: (FDesc rep key desc, Elt val)
+       => desc
+       -> (Exp val -> Exp val -> Exp val)
+       -> Exp val
+       -> Acc (Table rep key val)
+       -> Acc (Table (FRRes' rep desc) (FKRes' key desc) val)
+fold'' = undefined
+
+
+
+-- class (Rep rep key, IsKey key, RepShape rep key ~ KeyShape key) => Fold' rep key where
   
-  type RepShape rep key :: RepR
+--   type RepShape rep key :: RepR
 
-  -- type FoldKeepKeyRes key
-  -- type FoldKeepRepRes rep
-  -- type FoldGroupKeyRes key
-  -- type FoldGroupRepRes rep
+--   -- type FoldKeepKeyRes key
+--   -- type FoldKeepRepRes rep
+--   -- type FoldGroupKeyRes key
+--   -- type FoldGroupRepRes rep
 
-  type FoldRepRes' rep desc
-  type FoldKeyRes' key desc
+--   type FoldRepRes' rep desc
+--   type FoldKeyRes' key desc
 
-  -- type PeelRep rep
-  -- type PeelKey key
+--   -- type PeelRep rep
+--   -- type PeelKey key
 
 
-  foldMeta' :: (FoldDescriptor'' desc (RepShape rep key))
-           => FoldDescriptor''' desc (RepShape rep key)
-           -> Acc (Meta rep key)
-           -> Acc ( Meta (FoldRepRes' rep desc) (FoldKeyRes' key desc)
-                  , A.Segments Int
-                  )
+--   foldMeta' :: (FoldDescriptor'' desc (RepShape rep key))
+--            => FoldDescriptor''' desc (RepShape rep key)
+--            -> Acc (Meta rep key)
+--            -> Acc ( Meta (FoldRepRes' rep desc) (FoldKeyRes' key desc)
+--                   , A.Segments Int
+--                   )
 
-  remainsRep :: (FoldDescriptor'' desc (RepShape rep key))
-             => FoldDescriptor''' desc (RepShape rep key)
-             -> Acc (Meta rep key)
-             -> Dict' (Fold' (FoldRepRes' rep desc)) (FoldKeyRes' key desc)
+--   remainsRep :: (FoldDescriptor'' desc (RepShape rep key))
+--              => FoldDescriptor''' desc (RepShape rep key)
+--              -> Acc (Meta rep key)
+--              -> Dict' (Fold' (FoldRepRes' rep desc)) (FoldKeyRes' key desc)
 
-  -- type IsDesc rep key desc :: Constraint
+--   -- type IsDesc rep key desc :: Constraint
 
-instance Fold' Z Z where
+-- instance Fold' Z Z where
 
-  type RepShape Z Z = RepZ
+--   type RepShape Z Z = RepZ
 
-  -- type FoldKeepKeyRes Z = Z
-  -- type FoldKeepRepRes Z = Z
-  -- Should never occur.
-  -- type FoldGroupKeyRes Z = Z
-  -- type FoldGroupRepRes Z = Z
+--   -- type FoldKeepKeyRes Z = Z
+--   -- type FoldKeepRepRes Z = Z
+--   -- Should never occur.
+--   -- type FoldGroupKeyRes Z = Z
+--   -- type FoldGroupRepRes Z = Z
 
-  type FoldRepRes' Z Keep = Z
-  type FoldKeyRes' Z Keep = Z
+--   type FoldRepRes' Z Keep = Z
+--   type FoldKeyRes' Z Keep = Z
 
-  -- type PeelRep Z = Z
-  -- type PeelKey Z = Z
+--   -- type PeelRep Z = Z
+--   -- type PeelKey Z = Z
 
-  foldMeta' FoldKeep' _ = T2 emptyMeta (A.fill (I1 1) 1)
-  -- foldMeta' (FoldGroup' _) _ = T2 emptyMeta (A.fill (I1 1) 1)
+--   foldMeta' FoldKeep' _ = T2 emptyMeta (A.fill (I1 1) 1)
+--   -- foldMeta' (FoldGroup' _) _ = T2 emptyMeta (A.fill (I1 1) 1)
 
-  remainsRep FoldKeep' _ = Dict'
+--   remainsRep FoldKeep' _ = Dict'
 
 
 -- type IsDesc' :: RepR -> Type -> Constraint
@@ -169,153 +232,153 @@ instance Fold' Z Z where
   -- type IsDesc Z Z Keep = ()
   -- type IsDesc Z Z _    = TypeError ('Text "")
 
-instance (Fold' rep keys, IndexKey key, IsKey key) => Fold' (rep :. Dense) (keys :. key) where
+-- instance (Fold' rep keys, IndexKey key, IsKey key) => Fold' (rep :. Dense) (keys :. key) where
   
-  type RepShape (rep :. Dense) (keys :. key) = RepSnoc (RepShape rep keys)
+--   type RepShape (rep :. Dense) (keys :. key) = RepSnoc (RepShape rep keys)
 
-  -- type FoldGroupKeyRes (keys :. key) = keys
-  -- type FoldGroupRepRes (rep :. Dense) = rep
+--   -- type FoldGroupKeyRes (keys :. key) = keys
+--   -- type FoldGroupRepRes (rep :. Dense) = rep
 
-  type FoldRepRes' (rep :. Dense) Keep = FoldRepRes' rep Keep :. Dense
-  type FoldRepRes' (rep :. Dense) (desc :. Group) = FoldRepRes' rep desc
+--   type FoldRepRes' (rep :. Dense) Keep = FoldRepRes' rep Keep :. Dense
+--   type FoldRepRes' (rep :. Dense) (desc :. Group) = FoldRepRes' rep desc
 
-  type FoldKeyRes' (keys :. key) Keep = FoldKeyRes' keys Keep :. key
-  type FoldKeyRes' (keys :. key) (desc :. Group) = FoldKeyRes' keys desc
+--   type FoldKeyRes' (keys :. key) Keep = FoldKeyRes' keys Keep :. key
+--   type FoldKeyRes' (keys :. key) (desc :. Group) = FoldKeyRes' keys desc
 
-  remainsRep FoldKeep' (Meta_ (T2 met _)) = case remainsRep FoldKeep' met of
-    Dict' -> Dict'
-  remainsRep (FoldGroup' rest) (Meta_ (T2 met _)) = case remainsRep rest met of
-    Dict' -> Dict' 
+--   remainsRep FoldKeep' (Meta_ (T2 met _)) = case remainsRep FoldKeep' met of
+--     Dict' -> Dict'
+--   remainsRep (FoldGroup' rest) (Meta_ (T2 met _)) = case remainsRep rest met of
+--     Dict' -> Dict' 
 
-  -- type PeelRep (rep :. Dense) = rep
-  -- type PeelKey (keys :. key) = keys
+--   -- type PeelRep (rep :. Dense) = rep
+--   -- type PeelKey (keys :. key) = keys
 
-  foldMeta' d dmet@(Meta_ (T2 met n)) =
-    case (d, remainsRep d dmet) of
-      (FoldKeep', Dict') -> case remainsRep FoldKeep' met of
-        Dict' -> 
-          let T2 met' seg = foldMeta' FoldKeep' met
-              len      = A.sum seg
-              seg'     = A.fill (I1 $ A.the len) (A.the n)
-          in  T2 (Meta_ $ T2 met' n) seg'
-      (FoldGroup' FoldKeep', Dict') ->
-        let T2 met' seg = foldMeta' FoldKeep' met
-            len         = A.sum seg
-            seg'        = A.fill (I1 $ A.the len) (A.the n)
-        in  T2 met' seg'
-      (FoldGroup' rest, Dict') ->
-        let T2 met' seg = foldMeta' rest met
-            seg'        = A.map (* A.the n) seg
-        in  T2 met' seg'
+--   foldMeta' d dmet@(Meta_ (T2 met n)) =
+--     case (d, remainsRep d dmet) of
+--       (FoldKeep', Dict') -> case remainsRep FoldKeep' met of
+--         Dict' -> 
+--           let T2 met' seg = foldMeta' FoldKeep' met
+--               len      = A.sum seg
+--               seg'     = A.fill (I1 $ A.the len) (A.the n)
+--           in  T2 (Meta_ $ T2 met' n) seg'
+--       (FoldGroup' FoldKeep', Dict') ->
+--         let T2 met' seg = foldMeta' FoldKeep' met
+--             len         = A.sum seg
+--             seg'        = A.fill (I1 $ A.the len) (A.the n)
+--         in  T2 met' seg'
+--       (FoldGroup' rest, Dict') ->
+--         let T2 met' seg = foldMeta' rest met
+--             seg'        = A.map (* A.the n) seg
+--         in  T2 met' seg'
 
 
-data RepR = RepZ | RepSnoc RepR
+-- data RepR = RepZ | RepSnoc RepR
 
-data SRepR r where
-  SRepZ :: SRepR RepZ
-  SRepSnoc :: SRepR r -> SRepR (RepSnoc r)
+-- data SRepR r where
+--   SRepZ :: SRepR RepZ
+--   SRepSnoc :: SRepR r -> SRepR (RepSnoc r)
 
--- type IsDesc :: RepR -> Type -> Bool
--- type family IsDesc r desc where
---   IsDesc (RepSnoc r) (desc :. Group) = IsDesc r desc
---   IsDesc _ Keep = True
+-- -- type IsDesc :: RepR -> Type -> Bool
+-- -- type family IsDesc r desc where
+-- --   IsDesc (RepSnoc r) (desc :. Group) = IsDesc r desc
+-- --   IsDesc _ Keep = True
 
--- type IsFoldDescriptor :: Type -> Type -> Constraint
--- type IsFoldDescriptor key desc = Assert (IsDesc (KeyShape key) desc)
---   ( TypeError (
---          ShowType desc 
---     :<>: Text " cannot be used as a fold descriptor for keys of type "
---     :<>: ShowType key)
-  -- )
--- TODO: maybe use typeerror library and IfStuck for better error message.
+-- -- type IsFoldDescriptor :: Type -> Type -> Constraint
+-- -- type IsFoldDescriptor key desc = Assert (IsDesc (KeyShape key) desc)
+-- --   ( TypeError (
+-- --          ShowType desc 
+-- --     :<>: Text " cannot be used as a fold descriptor for keys of type "
+-- --     :<>: ShowType key)
+--   -- )
+-- -- TODO: maybe use typeerror library and IfStuck for better error message.
 
-class FoldDescriptor'' desc dim where
+-- class FoldDescriptor'' desc dim where
   
-  -- getDescriptor' :: FoldDescriptor''' rep (FoldRepRes rep desc) key (FoldKeyRes key desc) desc
-  getDescriptor' :: FoldDescriptor''' desc dim
+--   -- getDescriptor' :: FoldDescriptor''' rep (FoldRepRes rep desc) key (FoldKeyRes key desc) desc
+--   getDescriptor' :: FoldDescriptor''' desc dim
 
-  -- getDict' :: Proxy rep -> Proxy key -> Proxy desc -> Dict' (Rep (FoldRepRes rep desc)) (FoldKeyRes key desc)
+--   -- getDict' :: Proxy rep -> Proxy key -> Proxy desc -> Dict' (Rep (FoldRepRes rep desc)) (FoldKeyRes key desc)
 
-instance FoldDescriptor'' Keep dim where
+-- instance FoldDescriptor'' Keep dim where
 
-  getDescriptor' = FoldKeep'
+--   getDescriptor' = FoldKeep'
 
-  -- getDict' _ _ _ = Dict'
+--   -- getDict' _ _ _ = Dict'
 
-instance (FoldDescriptor'' desc dim) => FoldDescriptor'' (desc :. Group) (RepSnoc dim) where
+-- instance (FoldDescriptor'' desc dim) => FoldDescriptor'' (desc :. Group) (RepSnoc dim) where
 
-  getDescriptor' = FoldGroup' getDescriptor'
+--   getDescriptor' = FoldGroup' getDescriptor'
 
 
--- instance (FoldDescriptor'' rep keys desc) => FoldDescriptor'' (rep :. r) (keys :. key) (desc :. Group) where
+-- -- instance (FoldDescriptor'' rep keys desc) => FoldDescriptor'' (rep :. r) (keys :. key) (desc :. Group) where
 
---   getDescriptor' = FoldGroup' _
+-- --   getDescriptor' = FoldGroup' _
    
 
 
-data FoldDescriptor''' desc dim where
-  FoldKeep' :: FoldDescriptor''' Keep dim
-  FoldGroup' :: (FoldDescriptor'' desc dim)
-             => FoldDescriptor''' desc dim
-             -> FoldDescriptor''' (desc :. Group) (RepSnoc dim)
+-- data FoldDescriptor''' desc dim where
+--   FoldKeep' :: FoldDescriptor''' Keep dim
+--   FoldGroup' :: (FoldDescriptor'' desc dim)
+--              => FoldDescriptor''' desc dim
+--              -> FoldDescriptor''' (desc :. Group) (RepSnoc dim)
 
-fold' :: forall rep key desc a
-      .  (Fold' rep key, FoldDescriptor'' desc (KeyShape key), Elt a)
-      => desc
-      -> (Exp a -> Exp a -> Exp a)
-      -> Exp a
-      -> Acc (Table rep key a)
-      -> Acc (Table (FoldRepRes' rep desc) (FoldKeyRes' key desc) a)
-fold' _ f e Table_ { meta_, vals_ } = 
-  let desc = getDescriptor' @desc @(KeyShape key)
-  in case remainsRep desc meta_  of
-       Dict' -> let T2 met' seg = foldMeta' desc meta_
-                in  Table_ met' $ A.foldSeg (combineMaybe f) (Just_ e) vals_ seg
+-- fold' :: forall rep key desc a
+--       .  (Fold' rep key, FoldDescriptor'' desc (KeyShape key), Elt a)
+--       => desc
+--       -> (Exp a -> Exp a -> Exp a)
+--       -> Exp a
+--       -> Acc (Table rep key a)
+--       -> Acc (Table (FoldRepRes' rep desc) (FoldKeyRes' key desc) a)
+-- fold' _ f e Table_ { meta_, vals_ } = 
+--   let desc = getDescriptor' @desc @(KeyShape key)
+--   in case remainsRep desc meta_  of
+--        Dict' -> let T2 met' seg = foldMeta' desc meta_
+--                 in  Table_ met' $ A.foldSeg (combineMaybe f) (Just_ e) vals_ seg
 
--- type family FoldKeyRes key desc where
---   FoldKeyRes key Keep = key
---   FoldKeyRes key Group = FoldGroupKeyRes key
+-- -- type family FoldKeyRes key desc where
+-- --   FoldKeyRes key Keep = key
+-- --   FoldKeyRes key Group = FoldGroupKeyRes key
 
--- type family FoldRepRes rep desc where
---   FoldRepRes rep Keep = rep
---   FoldRepRes rep Group = FoldGroupRepRes rep
+-- -- type family FoldRepRes rep desc where
+-- --   FoldRepRes rep Keep = rep
+-- --   FoldRepRes rep Group = FoldGroupRepRes rep
 
-class IsKey key where
+-- class IsKey key where
 
-  type KeyShape key :: RepR
+--   type KeyShape key :: RepR
 
-instance IsKey Z where
+-- instance IsKey Z where
 
-  type KeyShape Z = RepZ
+--   type KeyShape Z = RepZ
 
-instance (IsKey keys) => IsKey (keys :. key) where
+-- instance (IsKey keys) => IsKey (keys :. key) where
 
-  type KeyShape (keys :. key) = RepSnoc (KeyShape keys)
+--   type KeyShape (keys :. key) = RepSnoc (KeyShape keys)
 
-data Table' rep key val where
-  Table' :: (Rep rep key, Elt val) => Acc (Table rep key val) -> Table' rep key val
+-- data Table' rep key val where
+--   Table' :: (Rep rep key, Elt val) => Acc (Table rep key val) -> Table' rep key val
 
--- deriving instance Generic (Table' rep key val)
+-- -- deriving instance Generic (Table' rep key val)
 
--- instance Arrays (Table' rep key val) where
---   type ArraysR (Table' rep key val) = ArraysR (Table rep key val)
+-- -- instance Arrays (Table' rep key val) where
+-- --   type ArraysR (Table' rep key val) = ArraysR (Table rep key val)
 
---   arraysR = unsafeAssume @(Rep rep key, Elt val) $
---     arraysR @(Table rep key val)
+-- --   arraysR = unsafeAssume @(Rep rep key, Elt val) $
+-- --     arraysR @(Table rep key val)
 
---   fromArr (Table' tab) = fromArr tab
---   toArr tab = unsafeAssume @(Rep rep key, Elt val) $
---     Table' (toArr tab)
+-- --   fromArr (Table' tab) = fromArr tab
+-- --   toArr tab = unsafeAssume @(Rep rep key, Elt val) $
+-- --     Table' (toArr tab)
 
--- pattern Table'_ :: Acc (Table rep key val) -> Acc (Table' rep key val)
--- pattern Table'_ tab = Pattern tab
+-- -- pattern Table'_ :: Acc (Table rep key val) -> Acc (Table' rep key val)
+-- -- pattern Table'_ tab = Pattern tab
 
-unsafeAssume :: forall c r . (c => r) -> r
-unsafeAssume f = case (unsafeAxiom :: Dict'' c) of
-  Dict'' -> f
+-- unsafeAssume :: forall c r . (c => r) -> r
+-- unsafeAssume f = case (unsafeAxiom :: Dict'' c) of
+--   Dict'' -> f
 
-unsafeAxiom :: Dict'' c
-unsafeAxiom = unsafeCoerce (Dict'' :: Dict'' ())
+-- unsafeAxiom :: Dict'' c
+-- unsafeAxiom = unsafeCoerce (Dict'' :: Dict'' ())
 
 data Dict'' c where
   Dict'' :: c => Dict'' c
